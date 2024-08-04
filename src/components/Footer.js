860be6661ebe221cce9699158345c2bee82735f1
Pritam Sanjay S <107757690+pritamsps@users.noreply.github.com>
2024-08-04 21:01:30 +0530

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return <footer className="py-1 bg-dark">
-                <p className="text-center text-white mt-1 ">
-                    Menzo - 2024-2024, All Rights Reserved
-                </p>
-            </footer>
-}
