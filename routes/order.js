3330272c5dc61ae4afa9b427f058edd9c97a39f1
Pritam Sanjay S <107757690+pritamsps@users.noreply.github.com>
2024-08-04 21:01:16 +0530

    Delete routes directory

--- routes/order.js
+++
@@ -1,5 +0,0 @@
-const express=require('express');
-const { createorder } = require('../controllers/ordercontroller');
-const router=express.Router();
-router.route('/order').post(createorder);
-module.exports=router;
--- routes/product.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const { getProducts, getsingleProduct } = require('../controllers/productcontroller');
-const router=express.Router();
-router.route('/products').get(getProducts);
-
-router.route('/products/:id').get(getsingleProduct);
-module.exports=router;
