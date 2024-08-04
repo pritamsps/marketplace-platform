2307ddc803feb6824288d071df8ba8c24f9c2c85
Pritam Sanjay S <107757690+pritamsps@users.noreply.github.com>
2024-08-04 20:52:36 +0530

    Delete config directory

--- config/connectdbase.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-const connectdbase= ()=>{
-    mongoose.connect(process.env.DB_URL).then((con) => {
-        console.log("mongo db connected to host:"+con.connection.host)
-    })
-
-};
-module.exports=connectdbase;
